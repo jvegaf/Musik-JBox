5f048fc8126c15a2a1d8e6c1b0c613f2816ceebb
Jose Vega <josevega234@gmail.com>
2021-12-29 03:17:02 +0100

    refactor

--- src/main/java/me/jvegaf/musikbox/ui/components/Tracklist.java
+++ src/main/java/me/jvegaf/musikbox/ui/components/Tracklist.java
@@ -3,12 +3,15 @@ package me.jvegaf.musikbox.ui.components;
 import com.google.inject.Inject;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.ObservableList;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import me.jvegaf.musikbox.bus.CommandBus;
@@ -23,6 +26,9 @@ import java.util.ResourceBundle;
 
 public class Tracklist extends AnchorPane implements Initializable {
 
+    private final TracksRepository tracksRepository;
+    private final CommandBus commandHandler;
+    private final Logger logger = Logger.getLogger(Tracklist.class);
     @FXML
     private TableView<Track> songsTableView;
     @FXML
@@ -39,14 +45,6 @@ public class Tracklist extends AnchorPane implements Initializable {
     private TableColumn<Track, String> bpmColumn;
     @FXML
     private TableColumn<Track, String> yearColumn;
-
-
-    private Track selectedTrack;
-    private final TracksRepository tracksRepository;
-    private final CommandBus commandHandler;
-
-    private final Logger logger = Logger.getLogger(Tracklist.class);
-    private ObservableList<Track> seletedTracks;
     private TableView.TableViewSelectionModel<Track> selectionModel;
 
     @Inject
@@ -73,7 +71,6 @@ public class Tracklist extends AnchorPane implements Initializable {
 
         selectionModel = this.songsTableView.getSelectionModel();
         selectionModel.setSelectionMode(SelectionMode.MULTIPLE);
-        seletedTracks = selectionModel.getSelectedItems();
         titleColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         artistColumn.setCellValueFactory(new PropertyValueFactory<>("artist"));
         albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
@@ -94,9 +91,8 @@ public class Tracklist extends AnchorPane implements Initializable {
                 if (row.isEmpty()) {
                     return;
                 }
-                this.selectedTrack = row.getItem();
                 if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
-                    this.commandHandler.playTrack(this.selectedTrack);
+                    this.commandHandler.playTrack(this.selectionModel.getSelectedItem());
                 }
             });
             return row;
@@ -106,19 +102,28 @@ public class Tracklist extends AnchorPane implements Initializable {
     private ContextMenu getContextMenu() {
         ContextMenu menu = new ContextMenu();
         MenuItem fixallItem = new MenuItem();
-        fixallItem.textProperty()
-                  .bind(Bindings.createStringBinding(() -> "Fix " + seletedTracks.size() + " tracks", seletedTracks));
+        selectionModel.getSelectedItems().addListener((ListChangeListener<Track>) c -> {
+            if (selectionModel.getSelectedItems().size() > 1) {
+                fixallItem.setText("Fix " + selectionModel.getSelectedItems().size() + " tracks");
+            }
+            if (selectionModel.getSelectedItems().size() == 1) {
+                fixallItem.setText("Fix Track");
+            }
+        });
         fixallItem.setOnAction(actionEvent -> {
-            this.commandHandler.fixTags(this.seletedTracks);
+            this.commandHandler.fixTags(selectionModel.getSelectedItems());
             selectionModel.clearSelection();
         });
         MenuItem detailItem = new MenuItem("View Detail");
-        detailItem.setOnAction(actionEvent -> this.commandHandler.showTrackDetail(this.selectedTrack));
+        detailItem.setOnAction(actionEvent -> this.commandHandler.showTrackDetail(this.selectionModel.getSelectedItem()));
         detailItem.disableProperty()
-                  .bind(Bindings.createBooleanBinding(() -> this.seletedTracks.size() != 1, seletedTracks));
+                  .bind(Bindings.createBooleanBinding(() -> this.selectionModel.getSelectedItems().size() != 1,
+                                                      selectionModel.getSelectedItems()));
         MenuItem playItem = new MenuItem("Play Song");
-        playItem.disableProperty().bind(Bindings.createBooleanBinding(() -> seletedTracks.size() != 1, seletedTracks));
-        playItem.setOnAction(actionEvent -> this.commandHandler.playTrack(this.selectedTrack));
+        playItem.disableProperty()
+                .bind(Bindings.createBooleanBinding(() -> this.selectionModel.getSelectedItems().size() != 1,
+                                                    selectionModel.getSelectedItems()));
+        playItem.setOnAction(actionEvent -> this.commandHandler.playTrack(this.selectionModel.getSelectedItem()));
 
 
         menu.getItems().addAll(
@@ -130,5 +135,13 @@ public class Tracklist extends AnchorPane implements Initializable {
         return menu;
     }
 
+    private void addEventHandler(final Node keyNode) {
+        keyNode.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.SPACE) {
+                if (selectionModel.getSelectedItems().size() == 1) return;
+                this.commandHandler.playTrack(selectionModel.getSelectedItem());
+            }
+        });
+    }
 
 }
