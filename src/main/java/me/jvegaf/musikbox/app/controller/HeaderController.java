823830f8274e8bc77b36002f720cb60c83bab947
Jose Vega <josevega234@gmail.com>
2022-01-06 04:29:00 +0100

    styles

--- src/main/java/me/jvegaf/musikbox/app/controller/HeaderController.java
+++ src/main/java/me/jvegaf/musikbox/app/controller/HeaderController.java
@@ -1,13 +1,14 @@
 package me.jvegaf.musikbox.app.controller;
 
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import lombok.extern.log4j.Log4j2;
 import me.jvegaf.musikbox.app.player.MusicPlayer;
@@ -18,8 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 @Log4j2
 @Component
@@ -28,31 +27,35 @@ public final class HeaderController {
 
 
     @FXML
-    private Label artistLabel;
+    private       Label       artistLabel;
     @FXML
-    private Label titleLabel;
+    private       Label       titleLabel;
     @FXML
-    private Label currentTimeLabel;
+    private       Label       currentTimeLabel;
     @FXML
-    private Label remainTimeLabel;
+    private       Label       remainTimeLabel;
     @FXML
-    private ProgressBar progressBar;
+    private       ProgressBar progressBar;
     @FXML
-    private Button prevBtn;
+    private       Button      prevBtn;
     @FXML
-    private Button playBtn;
+    private       Button      playBtn;
     @FXML
-    private Button pauseBtn;
+    private       Button      pauseBtn;
     @FXML
-    private Button nextBtn;
+    private       Button      nextBtn;
     @FXML
-    public Button openFolderBtn;
+    public        Button      openFolderBtn;
+    private final MusicPlayer player;
+    private final CommandBus  bus;
+    @FXML
+    public        Button      closeBtn;
 
     private Double duration;
-
-    private MusicPlayer player;
-
-    private CommandBus bus;
+    @FXML
+    public  Button minimizeBtn;
+    @FXML
+    public  Button maximizeBtn;
 
     @Autowired
     public HeaderController(MusicPlayer player, CommandBus bus) {
@@ -63,6 +66,11 @@ public final class HeaderController {
     @FXML
     public void initialize() {
         disablePlayerControls(true);
+        closeBtn.setOnAction(event -> Platform.exit());
+        minimizeBtn.setOnAction(event -> ((Stage) ((Button) event.getSource()).getScene()
+                                                                              .getWindow()).setIconified(true));
+        maximizeBtn.setOnAction(event -> ((Stage) ((Button) event.getSource()).getScene().getWindow()).setMaximized(
+                winMaximized()));
         nextBtn.setOnMouseClicked(event -> System.out.println("next clicked !"));
         playBtn.setOnMouseClicked(event -> this.player.continuePlaying());
         pauseBtn.setOnMouseClicked(event -> this.player.pauseTrack());
@@ -71,6 +79,10 @@ public final class HeaderController {
         this.titleLabel.textProperty().bind(this.player.titleProperty);
         this.player.statusProperty.addListener((observable, oldValue, newValue) -> playerStatusHandler(newValue));
         openFolderBtn.setOnAction(event -> openActionListener());
+    }
+
+    private boolean winMaximized() {
+        return !((Stage) (maximizeBtn.getScene().getWindow())).isMaximized();
     }
 
 
@@ -83,30 +95,30 @@ public final class HeaderController {
     private void initDisplayControls() {
 
         initProgressBar();
+        initDisplayLabels();
+    }
+
+    private void initDisplayLabels() {
         currentTimeLabel.textProperty()
-                        .bind(Bindings.createStringBinding(() -> String.format("%.0f:%02.0f", player
-                                                                   .getMediaPlayer()
-                                                                   .getCurrentTime()
-                                                                   .toMinutes(), player
-                                                                                       .getMediaPlayer()
-                                                                                       .getCurrentTime()
-                                                                                       .toSeconds() % 60),
+                        .bind(Bindings.createStringBinding(() -> String.format("%.0f:%02.0f",
+                                                                               player.getMediaPlayer()
+                                                                                     .getCurrentTime()
+                                                                                     .toMinutes(),
+                                                                               player.getMediaPlayer()
+                                                                                     .getCurrentTime()
+                                                                                     .toSeconds() % 60),
                                                            player.getMediaPlayer().currentTimeProperty()));
 
         remainTimeLabel.textProperty()
-                       .bind(Bindings.createStringBinding(() -> String.format("%.0f:%02.0f", player
-                               .getMediaPlayer()
-                               .getCurrentTime()
-                               .toMinutes() - player
-                               .getMediaPlayer()
-                               .getTotalDuration()
-                               .toMinutes(), (player
-                               .getMediaPlayer()
-                               .getTotalDuration()
-                               .toSeconds() - player
-                               .getMediaPlayer()
-                               .getCurrentTime()
-                               .toSeconds()) % 60), player.getMediaPlayer().currentTimeProperty()));
+                       .bind(Bindings.createStringBinding(() -> String.format("%.0f:%02.0f", player.getMediaPlayer()
+                                                                                                   .getCurrentTime()
+                                                                                                   .toMinutes() - player.getMediaPlayer()
+                                                                                                                        .getTotalDuration()
+                                                                                                                        .toMinutes(), (player.getMediaPlayer()
+                                                                                                                                             .getTotalDuration()
+                                                                                                                                             .toSeconds() - player.getMediaPlayer()
+                                                                                                                                                                  .getCurrentTime()
+                                                                                                                                                                  .toSeconds()) % 60), player.getMediaPlayer().currentTimeProperty()));
     }
 
     private void initProgressBar() {
