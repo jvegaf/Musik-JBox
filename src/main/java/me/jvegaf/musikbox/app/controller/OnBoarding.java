dd4a1a9ec32bd3638ae03ff9099942480011c0f0
Jose Vega <josevega234@gmail.com>
2022-01-14 14:45:44 +0100

    Onboarding modal window

---
+++ src/main/java/me/jvegaf/musikbox/app/controller/OnBoarding.java
@@ -0,0 +1,10 @@
+package me.jvegaf.musikbox.app.controller;
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView
+public final class OnBoarding {
+
+}
